a23c6bcd78d4c33791ee46eeac6b848c88bddea7
Cristhian Hernandez <cristhi_her2000@hotmail.com>
2017-08-07 16:24:51 -0500

    Create pi-calc.py

---
+++ pi-calc.py
@@ -0,0 +1,14 @@
+import math
+pie = 0
+drange1 = list(range(1,22000,2))
+drange2 = drange1[1::2]
+
+for i in drange2:  # We don't want repeated digits
+    if i in drange1:
+        drange1.remove(i)
+
+for i1, i2 in zip(drange1, drange2):  # This is the Pi formula
+    pie += (4/i1)-(4/i2)
+
+print("Calculated Pi: ", round(pie,4))  # Only 1st 4 digits are accurate
+print("Python's math.pi: ", math.pi)
